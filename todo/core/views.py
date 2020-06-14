a638ce95cda9d473e12b8e7c9ff18fdd512ffc71
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-06-14 17:19:09 -0300

    feat(auth): check for authorization in views

--- todo/core/views.py
+++ todo/core/views.py
@@ -1,19 +1,30 @@
 from django.contrib.auth.models import User
+from django.db.models import get_object_or_404
 
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import viewsets, generics
+from rest_framework.permissions import BasePermission, IsAuthenticated, AllowAny
 
 from .models import Todo
 from .serializers import TodoSerializer, UserSerializer
 
+class IsAuthorized(BasePermission):
+    message = 'Unauthorized user'
+
+    def has_object_permission(self, request, view, obj):
+        if type(obj) is User:
+            return obj == request.user
+
+        return obj.created_by == request.user
+
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAuthorized]
 
-class UserViewSet(viewsets.ModelViewSet):
+    def get_queryset(self):
+        return Todo.objects.filter(created_by=self.request.user)
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
+    permission_classes = [IsAuthenticated, IsAuthorized]
